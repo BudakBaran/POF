cce96f48b5d2543b6005bc607d9d9166e92ebc0a
Baran Budak <baranbudak19973344@gmail.com>
2020-01-18 13:22:15 +0300

    Flex change to 2018 version

---
+++ UnityFor2018/Assets/NVIDIA/Flex/Helpers/FlexFluidRenderer.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NVIDIA.Flex
+{
+    [ExecuteInEditMode]
+    [DisallowMultipleComponent]
+    [AddComponentMenu("NVIDIA/Flex/Flex Fluid Renderer")]
+    public class FlexFluidRenderer : MonoBehaviour
+    {
+        #region Messages
+        void Start()
+        {
+            _simuData = GetComponent<simuData>();
+        }
+
+        void OnDisable()
+        {
+ 
+        }
+
+        void Update()
+        {
+
+        }
+        #endregion
+
+        #region Private
+
+        public Vector4 particle;
+
+        public Vector4 get()
+        {
+            return particle;
+        }
+
+        #endregion
+        simuData _simuData;
+      
+
+    }
+}
