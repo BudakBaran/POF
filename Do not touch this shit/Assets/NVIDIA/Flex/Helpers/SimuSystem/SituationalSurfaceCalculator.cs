ca9ecbb8d9e59a58a30f8aa22ec64cc0d2d21e50
BarBud <bdk.baran@gmail.com>
2020-04-12 14:31:42 +0300

    all kindz of data included

---
+++ Do not touch this shit/Assets/NVIDIA/Flex/Helpers/SimuSystem/SituationalSurfaceCalculator.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SituationalSurfaceCalculator
+{
+
+    public Vector4[] particles;
+    public HashSystem.HashModel[] groups;
+    Bounds bounds;
+    Bounds particleNeighbound;
+
+    //////////////////////////////// General FunctionS   ////////////////////////////////////////
+    /*
+        public float findDistance(Vector3 vertex, Vector3 point)
+        {
+            return (float)Math.Sqrt(Math.Pow((vertex.x - point.x), 2) + Math.Pow((vertex.y - point.y), 2) + Math.Pow((vertex.z - point.z), 2));
+        }
+    */
+
+
+    public float findDistance(Vector3 vertex, Vector3 point)
+    {
+        float xDimension = vertex.x - point.x;
+        float yDimension = vertex.y - point.y;
+        float zDimension = vertex.z - point.z;
+
+        xDimension = (float)Math.Pow(xDimension, 2);
+        yDimension = (float)Math.Pow(yDimension, 2);
+        zDimension = (float)Math.Pow(zDimension, 2);
+
+
+        return (float)Math.Sqrt(xDimension + yDimension + zDimension);
+    }
+
+    //////////////////////////////// SURFACE FINDING PART   ////////////////////////////////////////
+
+
+    public float findConstant(float radius)
+    {
+        float cons = 8 / (float)(Math.PI);
+        return cons;
+    }
+
+    public float findGradientWeight(Vector3 particle, Vector3 neighbour, float radius)
+    {
+        //////////////// IF RETURN NEGATİVE ERROR
+        float statcons = findConstant(radius);
+        float q = findDistance(particle, neighbour) / radius;
+        float gradient = 0;
+        if (0 <= q && q < 0.5)
+        {
+            gradient = (-2 * q) + (3 * q * q / 2);
+        }
+        else if (0.5 <= q && q <= 1)
+        {
+            gradient = (-1 / 2) * (float)Math.Pow((2 - q), 2);
+        }
+        else if (q > 1)
+        {
+            gradient = 0;
+        }
+        gradient *= (statcons / (float)Math.Pow(radius, 3));
+        return gradient;
+    }
+
+
+    public List<int> findNeigbourParticles(int[] neighbourCells)
+    {
+        List<int> neighbours = new List<int>();
+        for (int i = 0; i < neighbourCells.Length; i++)
+        {
+            if (groups[neighbourCells[i]] != null)
+            {
+                for (int j = 0; j < groups[neighbourCells[i]].pointIndice.Length; j++)
+                {
+                    if (groups[neighbourCells[i]].pointIndice[j] != -1)
+                    {
+                        neighbours.Add(groups[neighbourCells[i]].pointIndice[j]);
+                    }
+                }
+            }
+        }
+        return neighbours;
+    }
+
+    public int[] findSurfaceParticles(int particleId, int[] neighbours)
+    {
+        return null;
+
+    }
+
+    //////////////////////////////// Zhu & Bridson Part   /////////////////////////////////////////////////////////////////
+
+/*
+
+    public float findKernel(float s)
+    {
+        return Math.Max(0, (float)Math.Pow(1 - Math.Pow(s, 2), 3));
+    }
+
+*/
+
+
+    public float findKernel(float s)
+    {
+        float q=s;
+        q = (float)Math.Pow(1 - Math.Pow(q , 2) , 3);
+        return Math.Max(0 , q);
+    }
+
+    public float[] findWeights(Vector3 vertex, Vector3[] particles, float[] weights, float length)
+    {
+        float sum = 0;
+        for (int j = 0; j < particles.Length; j++)
+        {
+            sum += findKernel(findDistance(vertex, particles[j]) / length);
+        }
+        for (int i = 0; i < particles.Length; i++)
+        {
+            weights[i] = findKernel(findDistance(vertex, particles[i]) / length) / sum;
+        }
+        return weights;
+    }
+
+    public Vector3 weightedPos(Vector3[] particles, float[] weights)
+    {
+        Vector3 ret = new Vector3(0, 0, 0);
+        for (int i = 0; i < particles.Length; i++)
+        {
+            ret += particles[i] * weights[i];
+        }
+        return ret;
+    }
+
+
+
+    /*
+    public float ZhuAndBridson(Vector3 vertex, Vector3 weighted, float radius)
+    {
+        return (float)Math.Sqrt(Math.Pow((vertex.x - weighted.x), 2) + Math.Pow((vertex.y - weighted.y), 2) + Math.Pow((vertex.z - weighted.z), 2)) - radius;
+    }
+*/
+
+    public float zhuAndBridson(Vector3 vertex, Vector3 weighted, float radius)
+    {
+        float xDimension = (vertex.x - weighted.x);
+        float yDimension = (vertex.y - weighted.y);
+        float zDimension = (vertex.z - weighted.z);
+
+        xDimension = (float)Math.Pow(xDimension, 2);
+        yDimension = (float)Math.Pow(yDimension, 2);
+        zDimension = (float)Math.Pow(zDimension, 2);
+
+        return (float)Math.Sqrt((xDimension + yDimension + zDimension)) - radius;
+    }
+
+}
---
+++ Do not touch this shit/Assets/NVIDIA/Flex/Helpers/SimuSystem/SurfaceRecognition.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+// Change this class name as SurfaceParticleFinder.
+
+public class SurfaceRecognition
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public Vector4[] _particles;
+    public HashSystem.HashModel[] _groups;
+    Bounds _bounds;
+    Bounds _ParticleNeighbound;
+
+    float _radius;
+    public void SetData(Vector4[] particles, Bounds bounds, ref HashSystem.HashModel[] groups, float radius)
+    {
+        this._groups = groups;
+        this._particles = particles;
+        this._bounds = bounds;
+        this._radius = radius;
+    }
+
+    public float findKernel(float s)
+    {
+        float q = s * s;
+
+        q = (float)Math.Pow((1 - q), 3);
+
+        return Math.Max(0, q);
+    }
+
+    public float FindDistance(Vector3 vertex, Vector3 point)
+    {
+        return (float)Math.Sqrt(Math.Pow((vertex.x - point.x), 2) + Math.Pow((vertex.y - point.y), 2) + Math.Pow((vertex.z - point.z), 2));
+    }
+
+    public List<Vector3> FindNeigbourParticles(int [] neighbourCells)
+    {
+        List<Vector3> neighbours = new List<Vector3>();
+        for (int i = 0; i < neighbourCells.Length; i++)
+        {
+            if (neighbourCells[i] != -1) { 
+                if (_groups[neighbourCells[i]] != null)
+                {
+                    for (int j = 0; j < _groups[neighbourCells[i]].pointIndice.Length; j++)
+                    {
+                        if (_groups[neighbourCells[i]].pointIndice[j] != -1)
+                        {
+                            float x = _particles[_groups[neighbourCells[i]].pointIndice[j]].x;
+                            float y = _particles[_groups[neighbourCells[i]].pointIndice[j]].y;
+                            float z = _particles[_groups[neighbourCells[i]].pointIndice[j]].z;
+                            neighbours.Add(new Vector3(x, y, z));
+                        }
+                    }
+                }
+            }
+        }
+        return neighbours;
+    }
+
+
+    public Vector3 FindWeigtedX(Vector3 centerParticle, Vector3[] neighbourParticles, float searchRadius)
+    {
+        Vector3 weightedPosition = new Vector3(0, 0, 0);
+        float weightedConstant = 0;
+        
+        for (int i = 0; i < neighbourParticles.Length; i++)
+        {
+            float distance = FindDistance(centerParticle, neighbourParticles[i]);
+
+            weightedPosition += neighbourParticles[i] * findKernel(distance / searchRadius);
+
+            weightedConstant += findKernel(distance / searchRadius);
+        }
+
+        
+        return weightedPosition / weightedConstant;
+    }
+
+    public bool isSurfaceParticle(Vector3 centerParticle, Vector3[] neighbourParticles)
+    {
+        float returnVal = FindDistance(centerParticle, FindWeigtedX(centerParticle, neighbourParticles, _radius * 4));
+   
+        if (returnVal < 0.035)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public Bounds FindNeighbourArea(Vector4 particle)
+    {
+        Bounds insideCell = new Bounds();
+        float xMax = particle.x + _radius * 4;
+        if (xMax > _bounds.max.x)
+        {
+            xMax = _bounds.max.x;
+        }
+
+        float xMin = particle.x - _radius * 4;
+        if (xMin < _bounds.min.x)
+        {
+            xMin = _bounds.min.x;
+        }
+
+        float yMax = particle.y + _radius * 4;
+        if (yMax > _bounds.max.y)
+        {
+            yMax = _bounds.max.y;
+        }
+
+        float yMin = particle.y - _radius * 4;
+        if (yMin < _bounds.min.y)
+        {
+            yMin = _bounds.min.y;
+        }
+
+        float zMax = particle.z + _radius * 4;
+        if (zMax > _bounds.max.z)
+        {
+            zMax = _bounds.max.z;
+        }
+
+        float zMin = particle.z - _radius * 4;
+        if (zMin < _bounds.min.z)
+        {
+            zMin = _bounds.min.z;
+        }
+
+        insideCell.SetMinMax(new Vector3(xMin, yMin, zMin), new Vector3(xMax, yMax, zMax));
+
+        return insideCell;
+    }
+
+    // Get surface particles and return it
+    public int[] findBoundary()
+    {
+       
+        int[] neigbourCells = { -1 };
+        List<int> surfaceParticles = new List<int>();
+        Bounds insideCell = new Bounds();
+        for (int i = 0; i < 4096; i++)
+        {
+
+            insideCell = FindNeighbourArea(_particles[i]);
+
+            neigbourCells = FindAreaCells(insideCell);
+            bool isSurface = isSurfaceParticle(_particles[i], FindNeigbourParticles(neigbourCells).ToArray());
+            if (isSurface)
+            {
+                surfaceParticles.Add(i);
+            }
+        }
+        this._ParticleNeighbound = insideCell;
+        return surfaceParticles.ToArray();
+        //return neigbourCells;
+    }
+
+    //int[],
+
+        //  SETTER AND GETTER
+        
+    public Bounds GetParticleBound() 
+    {
+        return _ParticleNeighbound; 
+    }
+
+    public int[] FindAreaCells(Bounds insideCell) // Particle finder'a koy.
+    {
+        int _intervalx = (int)Math.Ceiling((_bounds.max.x - _bounds.min.x) / _radius);
+        int _intervaly = (int)Math.Ceiling((_bounds.max.y - _bounds.min.y) / _radius);
+        int topLeftBackward = FindID(new Vector3(insideCell.min.x, insideCell.max.y, insideCell.min.z));
+        int topLeftForward = FindID(new Vector3(insideCell.min.x, insideCell.max.y, insideCell.max.z));
+        int topRightBackward = FindID(new Vector3(insideCell.max.x, insideCell.max.y, insideCell.min.z));
+        int bottomLeftBackward = FindID(new Vector3(insideCell.min.x, insideCell.min.y, insideCell.min.z));
+
+        int tx = (topRightBackward - topLeftBackward),
+            ty = ((bottomLeftBackward - topLeftBackward) / _intervalx),
+            tz = ((topLeftForward - topLeftBackward) / (_intervalx * _intervaly));
+
+        int[] areaNums = Enumerable.Repeat(-1, ( (ty+1) * (tx+1) * (tz+1)) ).ToArray();
+        int i = 0, tempNum = topLeftBackward;
+
+        for (int k = 0; k < ty; k++)
+        {
+            for (int j = 0; j < tz; j++)
+            {
+                for (int m = 0; m < tx; m++)
+                {
+                    areaNums[i] = tempNum + m;
+                    i++;
+                }
+                tempNum += (_intervalx * _intervaly);
+            }
+            tempNum = areaNums[0] + (_intervalx * (k + 1));
+        }
+        return areaNums;
+    }
+
+    public int FindID(Vector3 insideCell) // Particle finder'a koy.
+    {
+        int cubeID;
+        int _intervalx = (int)Math.Ceiling((_bounds.max.x - _bounds.min.x) / (_radius));
+        int _intervaly = (int)Math.Ceiling((_bounds.max.y - _bounds.min.y) / (_radius));
+
+        int xId = (int)Math.Ceiling((insideCell.x - _bounds.min.x) / (_radius));
+        int yId = (int)Math.Ceiling((_bounds.max.y - insideCell.y) / (_radius));
+        int zId = (int)Math.Ceiling((insideCell.z - _bounds.min.z) / (_radius));
+        cubeID = (xId) + (_intervalx * (yId)) + (_intervalx * _intervaly * (zId));
+    
+        return cubeID;
+    }
+    public void ReturnSurfParticles()
+    {
+
+    }
+    public void CallGradientKernel(Vector3 particle, Vector3 neighbour, float radius)
+    {
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
